39e7b31ab8608dc96d2f3b958dbb41cd958bf656
Avokaado <ollippohjolainen@gmail.com>
2016-11-22 20:28:16 +0200

    Testing

--- test2.js
+++ test2.js
@@ -13,16 +13,11 @@ db.serialize(function() {
 
   var stmt = db.prepare("INSERT INTO Stuff VALUES (?, ?)");
 
-//Insert random data
-//  var rnd;
-//  for (var i = 0; i < 10; i++) {
-//    rnd = Math.floor(Math.random() * 10000000);
-    stmt.run(3, "Lorem");
-//  }
+  stmt.run(4, "Lorem ipsum");
   
 stmt.finalize();
 
-db.each("SELECT numero, thing FROM Stuff", function(err, row) {
+db.each("SELECT * FROM Stuff", function(err, row) {
     console.log(row.numero + ": " + row.thing);
   });
 
