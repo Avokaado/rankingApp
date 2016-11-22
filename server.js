c8cd4e838635ef7c687dfc85ee897beb1b95144b
Avokaado <ollippohjolainen@gmail.com>
2016-11-22 22:27:53 +0200

    Removed view.js and renamed folders

--- server.js
+++ server.js
@@ -8,21 +8,26 @@ server.connection({
 	host: 'localhost', 
 	port: 8000 });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!');
+server.register(require('inert'), (err) => {
+    if (err) {
+        throw err;
     }
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('./public/index.html');
+        }
+    });
 });
 
-server.route({
+/*server.route({
     method: 'GET',
     path: '/{name}',
     handler: function (request, reply) {
         reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
     }
-});
+});*/
 
 server.register(require('inert'), (err) => {
 
